cdd2e7d74e805b7f5334c2fa725241e33ed4446d
bustomii <tomblok.id@gmail.com>
2024-07-12 16:47:58 +0700

    update ui

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,11 +3,18 @@ import { Router } from '@angular/router';
 import { Login } from '../interface/login';  
 import { AuthService } from '../services/auth.service'
 import { FormBuilder, FormGroup, Validator } from '@angular/forms' ; 
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
