7851cf4270a89114d4312043daeb4a098c448621
bustomii <tomblok.id@gmail.com>
2024-07-16 16:57:42 +0700

    add app detail

--- src/app/apps/apps.component.ts
+++ src/app/apps/apps.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api/api.service';
 import { AppsSetting } from '../interface/apps-setting';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-apps',
@@ -9,8 +10,9 @@ import { AppsSetting } from '../interface/apps-setting';
   templateUrl: './apps.component.html',
   styleUrl: './apps.component.css'
 })
-export class AppsComponent {
-  constructor(private api: ApiService) {
+
+export class AppsComponent implements OnInit {
+  constructor(private api: ApiService, private route: ActivatedRoute) {
     this.getApp();
   }
 
@@ -27,11 +29,21 @@ export class AppsComponent {
     android: '',
     created_at: '',
     updated_at: ''
-  }
+  };
+
+  id: number = 0; 
 
   getApp() {
     this.api.appsSetting().subscribe((res:any) => {
       this.dataApp = res.data
+    });
+
+  }
+
+  
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
     });
   }
 }
